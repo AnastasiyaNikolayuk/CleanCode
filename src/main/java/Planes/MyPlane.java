a7f85945a0047abcecca027c372d63415c73e317
AnastasiyaNikolayuk <43971644+AnastasiyaNikolayuk@users.noreply.github.com>
2019-02-15 11:19:51 +0300

    Update MyPlane.java

--- src/main/java/Planes/MyPlane.java
+++ src/main/java/Planes/MyPlane.java
@@ -1,5 +1,7 @@
 package Planes;
 
+//incorrect name of class
+//there's no need in this one actually
 public class MyPlane  extends Plane {
     public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
